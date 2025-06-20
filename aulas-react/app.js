87b21d2593251060675f4456b851b68124c4b110
Anderson D Bernardo <anderson.danttas@hotmail.com>
2025-06-20 20:59:01 -0300

    Update

--- aulas-react/app.js
+++ aulas-react/app.js
@@ -1,1 +1,48 @@
-ReactDOM.render(<h1>Olá mundo com React!</h1>, document.getElementById('root'));
+//JSX
+/*const nome = 'Anderson Bernardo';
+const user = {
+    nome: 'Anderson',
+    sobrenome: 'Bernardo'
+};
+
+function nomeCompleto(user) {
+    return user.nome + ' ' + user.sobrenome;
+};
+
+const elemento = (
+    <div>
+        <h1 className="header">Olá, {nomeCompleto(user)}!</h1>
+        <p>Isto é um paragrafo.</p>
+    </div>
+);*/
+
+function BemVindo(props) {
+    if(props.nome) {
+        return <h1>Olá {props.nome}!</h1>
+    }
+    return <h1>Olá, Estranho!</h1>
+    
+};
+function Conteudo(props) {
+    return (
+        <div>
+            <h2>Subtitulo</h2>
+            <p>blablablablablablablablablalbs</p>
+        </div>
+    );
+};
+
+function App() {
+    return (
+        <div>
+            <BemVindo nome='Bernardo'/>
+            <Conteudo/>
+        </div>
+    );
+}
+
+//Basico do REACT
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
